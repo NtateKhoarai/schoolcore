7847649e686d4c96bc7454db7153b27ea55f30c1
Ntate Khoarai <ntatekhoarai@gmail.com>
2026-06-03 15:16:39 +0200

    Prepare for deployment

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:Bosalala2030*@localhost/schoolcore"
+DATABASE_URL = "postgresql://neondb_owner:npg_Fj9Pa6XWgiTA@ep-hidden-recipe-apmfwe2n.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require"
 
 engine = create_engine(DATABASE_URL)
 
